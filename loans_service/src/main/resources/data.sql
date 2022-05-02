6d8224d1da35eb05e2844b3edd127d1022026417
Gagan Sharma <gagansharma2717@gmail.com>
2022-05-02 12:53:51 +0530

    Created Loans Service

---
+++ loans_service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `loans`(`customer_id` ,`start_dt` ,`loan_type` ,`total_loan` ,`amount_paid`,`outstanding_amount` ,`create_dt`)
+VALUES(1,'2022-10-13','Home',2000000,500000,15000000,'2022-10-13');
+
+INSERT INTO `loans`(`customer_id` ,`start_dt` ,`loan_type` ,`total_loan` ,`amount_paid`,`outstanding_amount` ,`create_dt`)
+VALUES(1,'2022-10-6','Vehicle',400000,20000,200000,'2022-10-6');
+
+INSERT INTO `loans`(`customer_id` ,`start_dt` ,`loan_type` ,`total_loan` ,`amount_paid`,`outstanding_amount` ,`create_dt`)
+VALUES(1,'2022-10-22','Personal',500000,100000,400000,'2022-10-22');
