69e36d6e56d81372fa4e6bc746760743fbbb073a
Gagan Sharma <gagansharma2717@gmail.com>
2022-05-25 12:35:14 +0530

    Updated Code to get configurations from config server and created updated docker images

--- account-service/src/main/java/com/banking_service1/account_service/controller/AccountsController.java
+++ account-service/src/main/java/com/banking_service1/account_service/controller/AccountsController.java
@@ -1,12 +1,20 @@
 package com.banking_service1.account_service.controller;
 
+import com.banking_service1.account_service.config.AccountsServiceConfig;
 import com.banking_service1.account_service.model.Accounts;
 import com.banking_service1.account_service.model.Customer;
+import com.banking_service1.account_service.model.Properties;
 import com.banking_service1.account_service.repository.AccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 
 @RestController
 public class AccountsController {
@@ -14,15 +22,28 @@ public class AccountsController {
     @Autowired
     private AccountsRepository accountsRepository;
 
+    @Autowired
+    AccountsServiceConfig accountsConfig;
+
     @PostMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestBody Customer customer){
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
 
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-
-        if(accounts != null)
+        if(accounts != null) {
             return accounts;
-        else
+        } else {
             return null;
+        }
+
+    }
+
+    @GetMapping("/account/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountsConfig.getMsg(), accountsConfig.getBuildVersion(),
+                accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
+        String jsonStr = ow.writeValueAsString(properties);
+        return jsonStr;
     }
 
 }
