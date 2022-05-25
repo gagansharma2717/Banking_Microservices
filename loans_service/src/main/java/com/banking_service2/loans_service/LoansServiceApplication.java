dd3e34ba1db6045db6918a1153aa6c689d4edecb
Gagan Sharma <gagansharma2717@gmail.com>
2022-05-25 15:51:40 +0530

    Added RefreshScope

--- loans_service/src/main/java/com/banking_service2/loans_service/LoansServiceApplication.java
+++ loans_service/src/main/java/com/banking_service2/loans_service/LoansServiceApplication.java
@@ -3,11 +3,13 @@ package com.banking_service2.loans_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans(@ComponentScan("com.banking_service2.loans_service.controller"))
 @EnableJpaRepositories("com.banking_service2.loans_service.repository")
 @EntityScan("com.banking_service2.loans_service.model")
