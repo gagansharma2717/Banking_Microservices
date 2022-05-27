94935e47b14cb852d5129a46b44f91d652fcff24
Gagan Sharma <gagansharma2717@gmail.com>
2022-05-27 17:39:50 +0530

    Add EurekaServer and its configurations

--- cards_service/src/main/java/com/banking_service3/cards_service/CardsServiceApplication.java
+++ cards_service/src/main/java/com/banking_service3/cards_service/CardsServiceApplication.java
@@ -3,13 +3,11 @@ package com.banking_service3.cards_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RefreshScope
 @ComponentScans(@ComponentScan("com.banking_service3.cards_service.controller"))
 @EnableJpaRepositories("com.banking_service3.cards_service.repository")
 @EntityScan("com.banking_service3.cards_service.model")
